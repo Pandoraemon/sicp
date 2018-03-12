298c2578a0341d1aef060211559dc520d4826ebf
潘壮 <pandoraemon@saic.local>
2018-03-12 16:53:25 +0800

    2.67

--- code/chapter2/e2-3.scm
+++ code/chapter2/e2-3.scm
@@ -213,7 +213,18 @@
 		  ((< key (entry set)) (lookup key (left-branch set)))
 		  (else (lookup key (right-branch set)))))
 
+; 2.67
+(load "example.scm")
+(define sample-tree
+	(make-code-tree (make-leaf 'A 4)
+					(make-code-tree
+						(make-leaf 'B 2)
+						(make-code-tree
+							(make-leaf 'D 1)
+							(make-leaf 'C 1)))))
 
+(define sample-message '(0 1 1 0 0 1 0 1 0 1 1 1 0) )
+(decode sample-message sample-tree)
 
 
 
