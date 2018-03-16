c67c6037da39c1509719bd762b94b6fccf9b507d
pandoraemon <pz880304@163.com>
2018-03-16 14:50:39 +0800

    add create table

--- code/chapter3/example.scm
+++ code/chapter3/example.scm
@@ -113,7 +113,7 @@
 				queue)
 			  (else
 			  	(set-cdr! (rear-ptr queue) new-pair)
-			  	(set-rear-ptr queue new-pair)
+			  	(set-rear-ptr! queue new-pair)
 			  	queue))))
 
 (define (delete-queue! queue)
@@ -125,19 +125,59 @@
 
 
 
+; 一维表
+(define (lookup key table)
+	(let ((record (assoc key (cdr table))))
+		(if record
+			(cdr record)
+			false)))
+
+(define (assoc key records)
+	(cond ((null? records)
+			false)
+		  ((= key (caar records))
+		  	(car records))
+		  (else
+		  	(assoc key (cdr records)))))
 
 
+(define (insert! key value table)
+	(let ((record (assoc key (cdr table))))
+			(if record
+				(set-cdr! record value)
+				(set-cdr! table
+					(cons (cons key value) (cdr table)))))
+	'ok)
+
+(define (make-table)
+	(list '*table*))
 
 
+; 二维表
+(define (lookup key1 key2 table)
+	(let ((subtable (assoc key1 (cdr table))))
+		(if subtable
+			(let ((record (assoc key2 (cdr subtable))))
+				(if record
+					(cdr record)
+					false))
+			false)))
 
 
-
-
-
-
-
-
-
+(define (insert! key1 key2 value table)
+	(let ((subtable (assoc key1 (cdr table))))
+		(if subtable
+			(let ((record (assoc kye2 (cdr subtable))))
+				(if record
+					(set-cdr! record value)
+					(set-cdr! subtable
+								(cons (cons key2 value)
+									(cdr subtable)))))
+			(set-cdr! table
+						(cons (list key1
+									(cons key2 value))
+								(cdr table)))))
+	'ok)
 
 
 
