680f540b898bc5b09d9324ccc0867668af0832d1
pandoraemon <pz880304@163.com>
2018-03-19 17:13:36 +0800

    add 3-51

--- code/chapter3/infinity_stream.scm
+++ code/chapter3/infinity_stream.scm
@@ -4,3 +4,16 @@
 	(cons-stream n (integers-starting-from (+ n 1))))
 (define integers 
 	(integers-starting-from 1))
+
+(define (divisible? x y)
+	(= (remainder x y) 0))
+(define no-sevens
+	(stream-filter 
+		(lambda (x) (not (divisible? x 7)))
+		integers))
+(stream-ref no-sevens 100)
+
+(define (fibgen a b)
+	(cons-stream a (fibgen b (+ a b))))
+(define fibs (fibgen 0 1))
+(stream-ref fibs 5)
