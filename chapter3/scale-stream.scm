e243b1ea0bb329cd790e59964815466b9ba4d342
pandoraemon <pz880304@163.com>
2018-03-20 14:20:41 +0800

    add 3-56.scm

--- chapter3/scale-stream.scm
+++ chapter3/scale-stream.scm
@@ -1,6 +1,5 @@
-(load "3-50.scm")
 (define (scale-stream stream factor)
-	(stream-map (lambda (x) (* x factor) stream)))
+	(stream-map (lambda (x) (* x factor)) stream))
 
 (define double
 	(cons-stream 1 (scale-stream double 2)))
