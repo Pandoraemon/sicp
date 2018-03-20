30ed0a89b2026b26f0cae479d8773dc7f2dbfc08
pandoraemon <pz880304@163.com>
2018-03-20 15:09:49 +0800

    pi_stream.scm

---
+++ chapter3/pi_stream.scm
@@ -0,0 +1,12 @@
+(load "3-55.scm")
+(load "scale-stream.scm")
+(define (pi-summands n)
+	(cons-stream (/ 1.0 n)
+				 (stream-map - (pi-summands (+ n 2)))))
+
+(define pi-stream
+	(scale-stream
+		(partial-sums (pi-summands 1))
+		4))
+
+(stream-head pi-stream 10)
